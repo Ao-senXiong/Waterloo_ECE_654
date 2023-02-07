2e9da79d5002e2cc1fc791a046208269f51706a3
Aosen <aosenxiong@126.com>
2023-02-07 08:22:17 -0500

    test

--- first_assignment/functions.py
+++ first_assignment/functions.py
@@ -4,12 +4,14 @@ def check_length(node):
     if isinstance(node, ast.Name) and len(node.id) == 13:
         raise Exception("Identifier with length equal to 13 found")
 
+
 def check_nesting(node, depth=1):
     if depth > 4:
         raise Exception("Control structure nesting exceeds 4")
     if isinstance(node, (ast.If, ast.For, ast.While)):
         for child in ast.iter_child_nodes(node):
             check_nesting(child, depth + 1)
+
 
 def analyze_ast(source):
     tree = ast.parse(source)
