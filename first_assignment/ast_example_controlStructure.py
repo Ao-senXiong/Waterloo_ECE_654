a380c0fe96a743a7db96d2d0b1473dce5ecbcf0f
Aosen <aosenxiong@126.com>
2023-02-06 20:19:49 -0500

    First commit with working example

---
+++ first_assignment/ast_example_controlStructure.py
@@ -0,0 +1,13 @@
+def myfunction():
+    for x in y:
+        for x in y:
+            for x in y:
+                for x in y:
+                    for x in y:
+        # if(True):
+        #     if(True):
+        #         if(True):
+        #             if(True):
+                        return 1
+        else:
+            ...
